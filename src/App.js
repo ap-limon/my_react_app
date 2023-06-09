be05b23cc86ffc76ce446716e8286f11bf3b2df1
ap-limon <jnsapl5@gmail.com>
2023-06-09 18:15:23 +0600

    completed the temprature control app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const increment = () => setCount(count+1);
-  const decrease = () => setCount(count-1)
+  const [temprature, setTemprature] = useState(1);
+  const [tempratureColor, setTempratureColor] = useState("cold")
+
+  const tempColor = (v) => {
+    if (v < 20) { setTempratureColor("cold") };
+    if (v > 20) { setTempratureColor("neutral") };
+    if (v > 29) { setTempratureColor("hot") };
+  }
+
+  const increaseTemp = () => {
+    const newTemp = temprature + 1;
+    tempColor(newTemp);
+    setTemprature(newTemp)
+  }
+
+  const decreaseTemp = () => {
+    const newTemp = temprature - 1;
+    tempColor(newTemp);
+    setTemprature(newTemp);
+  }
+
   return (
-    <div className="App">
-      <button
-        onClick={increment} className="counter"
-      >
-        +
-      </button>
-      <button
-        onClick={decrease}
-        className="counter"
-      >
-        -
-      </button>
-      <h2>{count}</h2>
+    <div className="app-container">
+      <div className="temprature-display-container">
+        <div className={`temprature-display ${tempratureColor}`}>
+          {temprature} ℃
+        </div>
+      </div>
+      <div className="btn-container">
+        <button onClick={() => increaseTemp()} className="counter">
+          +
+        </button>
+        <button onClick={() => decreaseTemp()} className="counter">
+          -
+        </button>
+      </div>
     </div>
   );
 }
